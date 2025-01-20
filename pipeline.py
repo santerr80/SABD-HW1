704b115535c337e4024e907b2922811be8ce92e1
Александр Горбунов <santerr80@mail.ru>
2025-01-20 17:00:54 +0700

    UnGz

--- pipeline.py
+++ pipeline.py
@@ -1,8 +1,10 @@
 import luigi
-import requests
 import os
+import requests
 import tarfile
-
+import gzip
+import shutil
+from pathlib import Path
 
 class Download(luigi.Task):
     file_name = luigi.Parameter(default="GSE68849")
@@ -15,40 +17,70 @@ class Download(luigi.Task):
         os.makedirs(self.output_dir, exist_ok=True)
         url = f'https://www.ncbi.nlm.nih.gov/geo/download/?acc={self.file_name}&format=file'
         response = requests.get(url=url, timeout=50)
-        response.raise_for_status()  # Проверка на ошибки
+        response.raise_for_status()
+
         with open(self.output().path, 'wb') as f:
             f.write(response.content)
         print(f"Загрузка завершена и сохранена в {self.output().path}")
 
+        if not os.path.exists(self.output().path):
+            raise FileNotFoundError(f"Файл {self.output().path} не был создан.")
 
-class ExtractFiles(luigi.Task):
+class ExtractTar(luigi.Task):
     file_name = luigi.Parameter(default="GSE68849")
-    output_dir = luigi.Parameter(default="./downloads")
-
+    output_dir = luigi.Parameter(default="./extract")
+    
     def requires(self):
-        return Download(file_name=self.file_name, output_dir=self.output_dir)
-
+        return Download(file_name=self.file_name)
+    
     def output(self):
-        # Возвращаем один LocalTarget, который будет указывать на завершение задачи
-        return luigi.LocalTarget(os.path.join(self.output_dir, f"{self.file_name}_extracted.done"))
-
+        return luigi.LocalTarget(os.path.join(self.output_dir, f"{self.file_name}_tar_extracted.done"))
+    
     def run(self):
-        tar_path = self.requires().output().path
         os.makedirs(self.output_dir, exist_ok=True)
+        
+        with tarfile.open(self.requires().output().path, 'r') as tar:
+            members = tar.getmembers()
+            
+            for i, member in enumerate(members, 1):
+                subdir = os.path.join(self.output_dir, f"data{i}")
+                os.makedirs(subdir, exist_ok=True)
+                
+                member.name = os.path.basename(member.name)
+                tar.extract(member, path=subdir)
+                print(f"Файл {member.name} распакован в {subdir}")
+        
+        Path(self.output().path).touch()
+        print(f"Распаковка TAR завершена в {self.output_dir}")
 
-        with tarfile.open(tar_path, 'r') as tar:
-            members = tar.getnames()
-            for member in members:
-                # Извлекаем файл в соответствующую директорию
-                tar.extract(member, path=self.output_dir)
-                print(f"Файл {member} извлечен в {self.output_dir}")
-
-        # Создаем файл, чтобы обозначить, что задача завершена
-        with self.output().open('w') as f:
-            f.write('Extraction complete.')
-
-        print(f"Извлечение завершено. Все файлы сохранены в {self.output_dir}")
-
+class ExtractGz(luigi.Task):
+    file_name = luigi.Parameter(default="GSE68849")
+    output_dir = luigi.Parameter(default="./extract")
+    
+    def requires(self):
+        return ExtractTar(file_name=self.file_name)
+    
+    def output(self):
+        return luigi.LocalTarget(os.path.join(self.output_dir, f"{self.file_name}_gz_extracted.done"))
+    
+    def run(self):
+        # Проходим по всем поддиректориям data1, data2, ...
+        for subdir in Path(self.output_dir).glob('data*'):
+            # Ищем все .gz файлы в текущей поддиректории
+            for gz_file in subdir.glob('*.gz'):
+                output_file = gz_file.with_suffix('')  # Убираем расширение .gz
+                
+                # Распаковываем gz файл
+                with gzip.open(gz_file, 'rb') as f_in:
+                    with open(output_file, 'wb') as f_out:
+                        shutil.copyfileobj(f_in, f_out)
+                
+                # Удаляем исходный gz файл после распаковки
+                gz_file.unlink()
+                print(f"Файл {gz_file.name} распакован в {output_file.name}")
+        
+        Path(self.output().path).touch()
+        print(f"Распаковка GZ файлов завершена")
 
 if __name__ == '__main__':
-    luigi.run()
+    luigi.build([ExtractGz()], local_scheduler=True)
